67352c4c4da669109e5b4dcbe4015270c782429a
teodor-daniel1234 <teodor-daniel.balan@s.unibuc.ro>
2023-07-20 12:38:07 +0300

    Favicon + contact info
    Link-uri dinamice productie

--- curtea_brasoveana/src/App.js
+++ curtea_brasoveana/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';// for fontawesome icons
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';// for facebook icon
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';// for whatsapp icon
 function App() {
+  const Title = 'Curtea Brasoveana';
+  const PhoneNumber = "0732657369"
+  const Link = "https://www.facebook.com/PensiuneaCurteaBrasoveana/";
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${PhoneNumber}`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hai ca a mers!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{Title}</h1>
+
+      </div>
+      <div className="contact">
+        <a href={Link}><FontAwesomeIcon icon={faFacebook} /></a>
+        <a href={whatsappLink} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faWhatsapp} /></a>
+      </div>
     </div>
   );
 }
