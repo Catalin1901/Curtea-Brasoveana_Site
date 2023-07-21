3129aebe85fd79d5bd96eb081c5ee0dcda1991d7
teodor-daniel1234 <teodor-daniel.balan@s.unibuc.ro>
2023-07-21 14:29:10 +0300

    Random text function
    To test multiple dynamic formats

--- curtea_brasoveana/src/App.js
+++ curtea_brasoveana/src/App.js
@@ -2,9 +2,10 @@ import './styles/App.css';
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
-import Footer from './Components/Footer.js';
 import Navbar from './Components/Navbar.js';
-import hugeImage from './assets/Huge.png'; // Update the path based on your project structure
+import Main from './Components/Main.js';
+import Footer from './Components/Footer.js';
+
 
 
 function App() {
@@ -16,35 +17,7 @@ function App() {
       <Navbar />
       <div className="content">
         <h1>{Title}</h1>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-xxl-12 custom-border">
-              <img src={hugeImage} alt="Huge Image" className="img-fluid" />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-4 custom-border">
-              <p>Column 1</p>
-            </div>
-            <div className="col-4 custom-border">
-              <p>Column 2</p>
-            </div>
-            <div className="col-4 custom-border">
-              <p>Column 3</p>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-8 custom-border">
-              <p>Column 1</p>
-            </div>
-            <div className="col-4 custom-border">
-              <p>Column 2</p>
-            </div>
-          </div>
-
-        </div>
+        <Main />
         
       </div>
       <Footer />
