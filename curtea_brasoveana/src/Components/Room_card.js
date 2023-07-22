a57b35bf85f895a581dbc14c1536e9a1e5b9b0fe
Catalin1901 <catacatalin19012003@yahoo.com>
2023-07-22 20:57:03 +0300

    Scss try

--- curtea_brasoveana/src/Components/Room_card.js
+++ curtea_brasoveana/src/Components/Room_card.js
@@ -4,17 +4,15 @@ import Camera_dubla from '../assets/Camera_dubla.jpg';
 import Camera_tripla from '../assets/Camera_tripla.jpg';
 import Camera_qvadrupla from '../assets/Camera_qvadrupla.jpg';
 import Suite from '../assets/Suite.jpg';
-import '../styles/room_cards.css';
+import '../styles/test.css';
 
 function GroupExample() {
   return (
     <div>
-        <div>
-            <p className="TEO">ASDHJKASBDJKHABSDJKHABSDKJHBASDJKHBASJKDHBASJDHBASDJHB</p>
-        </div>
+        
     <CardGroup>
       <Card>
-        <Card.Img className="card-img" variant="top" src ={Camera_dubla} />
+        <Card.Img  variant="top" src ={Camera_dubla} />
         <Card.Body>
           <Card.Title>Camera Dubla</Card.Title>
           <Card.Text>
