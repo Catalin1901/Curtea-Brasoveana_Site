26d24dca0eb37cf231544ed9a96d57e0c3feab57
teodor-daniel1234 <teodor-daniel.balan@s.unibuc.ro>
2023-07-22 17:38:52 +0300

    Add card to carousel

--- curtea_brasoveana/src/Components/Carousel.js
+++ curtea_brasoveana/src/Components/Carousel.js
@@ -1,35 +1,40 @@
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { Image } from 'react-bootstrap';
 import FirstSlideImage from '../assets/Front.jpeg';
 import SecondSlideImage from '../assets/Front2.jpeg';
 import ThirdSlideImage from '../assets/Front3.jpeg';
+import '../styles/carousel.css';
 
-function UncontrolledExample() {
+function CardWithCarousel() {
   return (
-    <Carousel fade>
-      <Carousel.Item>
-        <Image src={FirstSlideImage} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image src={SecondSlideImage} alt="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image src={ThirdSlideImage} alt="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="card" style={{ width: '400px', height: '400px' }}>
+        <h5 class="card-title">Card title</h5>
+      <Carousel fade>
+        <Carousel.Item>
+          <Image src={FirstSlideImage} alt="First slide" className="carousel-image" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src={SecondSlideImage} alt="Second slide" className="carousel-image" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src={ThirdSlideImage} alt="Third slide" className="carousel-image" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 
-export default UncontrolledExample;
+export default CardWithCarousel;
--- curtea_brasoveana/src/pages/Rooms.js
+++ curtea_brasoveana/src/pages/Rooms.js
@@ -1,4 +1,5 @@
 import Carousel from '../Components/Carousel.js';
+import '../styles/rooms.css'
 const Rooms = () => {
     return (
      <div>
