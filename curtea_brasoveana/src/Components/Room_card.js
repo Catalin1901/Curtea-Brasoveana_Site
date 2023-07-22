839f8d64390c4653abf4fddb2f13855240dbdb36
Catalin1901 <catacatalin19012003@yahoo.com>
2023-07-22 20:39:19 +0300

    Room cards added
    Initial add of room cards, styling not done

--- curtea_brasoveana/src/Components/Room_card.js
+++ curtea_brasoveana/src/Components/Room_card.js
@@ -4,12 +4,17 @@ import Camera_dubla from '../assets/Camera_dubla.jpg';
 import Camera_tripla from '../assets/Camera_tripla.jpg';
 import Camera_qvadrupla from '../assets/Camera_qvadrupla.jpg';
 import Suite from '../assets/Suite.jpg';
+import '../styles/room_cards.css';
 
 function GroupExample() {
   return (
+    <div>
+        <div>
+            <p className="TEO">ASDHJKASBDJKHABSDJKHABSDKJHBASDJKHBASJKDHBASJDHBASDJHB</p>
+        </div>
     <CardGroup>
       <Card>
-        <Card.Img variant="top" src ={Camera_dubla} style={{ width: 'auto', height: '45vh' }}   />
+        <Card.Img className="card-img" variant="top" src ={Camera_dubla} />
         <Card.Body>
           <Card.Title>Camera Dubla</Card.Title>
           <Card.Text>
@@ -21,7 +26,7 @@ function GroupExample() {
         </Card.Footer>
       </Card>
       <Card>
-        <Card.Img variant="top" src={Camera_tripla} style={{ width: 'auto', height: '45vh' }}  />
+        <Card.Img variant="top" src={Camera_tripla}  />
         <Card.Body>
           <Card.Title>Camera Tripla</Card.Title>
           <Card.Text>
@@ -32,8 +37,11 @@ function GroupExample() {
           <small className="text-muted">Last updated 3 mins ago</small>
         </Card.Footer>
       </Card>
+      </CardGroup>
+
+      <CardGroup>
       <Card>
-        <Card.Img variant="top" src={Camera_qvadrupla} style={{ width: 'auto', height: '45vh' }} />
+        <Card.Img variant="top" src={Camera_qvadrupla} />
         <Card.Body>
           <Card.Title>Camera Qvuadrupla</Card.Title>
           <Card.Text>
@@ -45,7 +53,7 @@ function GroupExample() {
         </Card.Footer>
       </Card>
       <Card>
-        <Card.Img variant="top" src={Suite} style={{ width: 'auto', height: '45vh' }}  />
+        <Card.Img variant="top" src={Suite} />
         <Card.Body>
           <Card.Title>Suite</Card.Title>
           <Card.Text>
@@ -57,6 +65,7 @@ function GroupExample() {
         </Card.Footer>
       </Card>
     </CardGroup>
+    </div>
   );
 }
 
