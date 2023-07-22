b080535ffbbca724e67982c84535e096b7646eb4
Catalin1901 <catacatalin19012003@yahoo.com>
2023-07-23 00:10:13 +0300

    Map

--- curtea_brasoveana/src/Components/Contact.js
+++ curtea_brasoveana/src/Components/Contact.js
@@ -1,19 +1,47 @@
 import '../../src/styles/contact.css'
+import GoogleMapReact from '@react-google-maps'
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/map-marker'
 
-function Contact()
-{
-    return(
-        <div className = "contact-container">
-            <div className = "Date de contact">
+
+const location = {
+    address: 'Strada Băilor 16, Brașov 500123',
+    lat: 45.6365891,
+    lng: 25.5778443
+}
+
+const LocationPin = ({ text }) => (
+    <div className="pin">
+      <Icon icon={locationIcon} className="pin-icon" />
+      <p className="pin-text">{text}</p>
+    </div>
+  )
+
+function Contact() {
+    return (
+        <div className="contact-container">
+            <div className="Date de contact">
                 <h1>Date de contact</h1>
                 <p>Adresa: Strada Republicii nr. 56, Brașov 500030</p>
                 <p>Telefon: 0268 477 477</p>
                 <p>Email: asdasd@yahoo.com</p>
             </div>
-            <div className = "location">
+            <div className="location">
                 <h1>Locatie</h1>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.708639624831!2d25.57784427677371!3d45.63658912191938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b35b68a1ae9103%3A0x20c570eb379b5e75!2zQ3VydGVhIEJyYciZb3ZlYW7Egw!5e0!3m2!1sro!2sro!4v1690058769155!5m2!1sro!2sro" className = "Maps"></iframe>
+                <div className="google-map">
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: '' }}
+                        defaultCenter={location}
+                        defaultZoom={17}
+                    >
+                        <LocationPin
+                            lat={location.lat}
+                            lng={location.lng}
+                            text={location.address}
+                        />
+                    </GoogleMapReact>
                 </div>
+            </div>
 
         </div>
 
