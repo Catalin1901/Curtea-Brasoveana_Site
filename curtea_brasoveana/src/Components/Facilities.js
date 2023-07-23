9d70581de916e57e3e5c01379209f2ec7f7743fd
Catalin1901 <catacatalin19012003@yahoo.com>
2023-07-24 02:20:25 +0300

    Facilities terminat
    Facilities terminate full responsive

--- curtea_brasoveana/src/Components/Facilities.js
+++ curtea_brasoveana/src/Components/Facilities.js
@@ -3,77 +3,50 @@ import Camera_promote from '../assets/Camera_promote.jpg';
 import Gratar from '../assets/Gratar.jpg';
 import mic_dejun from '../assets/mic_dejun.jpg';
 import sauna from '../assets/sauna.jpg';
-import React, { useState } from 'react';
 
-
-const ImageGallery = () => {
-    const [hoveredItem, setHoveredItem] = useState(null);
-  
-    const handleMouseEnter = (index) => {
-      setHoveredItem(index);
-    };
-  
-    const handleMouseLeave = () => {
-      setHoveredItem(null);
-    };
-
-
-
-      
-        return (
-          <div className="gallery-container">
-            <div
-              className="gallery-item"
-              onMouseEnter={() => handleMouseEnter(1)}
-              onMouseLeave={handleMouseLeave}
-            >
-              <img src={Camera_promote} alt="Image 1" />
-              <div className={`overlay ${hoveredItem === 1 ? 'visible' : ''}`}>
-                <p className="text">
-                  {hoveredItem === 1 ? 'Va asteptam sa va petreceti sederea in Brasov in camerele noastre confortabile si spatioase' : 'Camere'}
-                </p>
-              </div>
-            </div>
-            <div
-              className="gallery-item"
-              onMouseEnter={() => handleMouseEnter(2)}
-              onMouseLeave={handleMouseLeave}
-            >
-              <img src={Gratar} alt="Image 2" />
-              <div className={`overlay ${hoveredItem === 2 ? 'visible' : ''}`}>
-                <p className="text">
-                  {hoveredItem === 2 ? 'Text on Hover for Image 2' : 'Initial Text for Image 2'}
-                </p>
-              </div>
-            </div>
-            <div
-              className="gallery-item"
-              onMouseEnter={() => handleMouseEnter(3)}
-              onMouseLeave={handleMouseLeave}
-            >
-              <img src={mic_dejun} alt="Image 3" />
-              <div className={`overlay ${hoveredItem === 3 ? 'visible' : ''}`}>
-                <p className="text">
-                  {hoveredItem === 3 ? 'Text on Hover for Image 3' : 'Initial Text for Image 3'}
-                </p>
-              </div>
-            </div>
-            <div
-              className="gallery-item"
-              onMouseEnter={() => handleMouseEnter(4)}
-              onMouseLeave={handleMouseLeave}
-            >
-              <img src={sauna} alt="Image 4" />
-              <div className={`overlay ${hoveredItem === 4 ? 'visible' : ''}`}>
-                <p className="text">
-                  {hoveredItem === 4 ? 'Text on Hover for Image 4' : 'Initial Text for Image 4'}
-                </p>
-              </div>
+function Facilities() {
+    return (
+        <div>
+        <div className="facilities-intro">
+            <h1 className="facilities-title">Facilitati</h1>
+            <p className="facilities-text">Curtea Brasoveana va ofera o gama larga de facilitati pentru a va face sederea cat mai placuta. Punem pe primul loc confortul in sejurul dumneavoastra
+            astfel ca incercam sa va asiguram tot ce aveti nevoie pentru a fi pe deplin satisfacuti la plecare si cu gandul de a reveni la noi</p>
+        </div>
+        <div className="gallery-container">
+          <div className="gallery-item">
+            <img src={Camera_promote} alt="Camere" />
+            <div className="overlay">
+              <p className="text">Camere</p>
+              <p className="text-hover">Curtea Brasoveana va asteapta in una din cele 20 de camere spatioase si comode pentru a va petrece sederea in Brasov.</p>
             </div>
           </div>
-        );
-      
-      
+          <div className="gallery-item">
+            <img src={Gratar} alt="Terasa" />
+            <div className="overlay">
+              <p className="text">Terasa</p>
+              <p className="text-hover">In curtea interioara va veti putea petrece serile facand un gratar sau jucand jocurile preferate alaturi de cei dragi.</p>
+
+            </div>
+          </div>
+          <div className="gallery-item">
+            <img src={mic_dejun} alt="Mic Dejun" />
+            <div className="overlay">
+              <p className="text">Mic Dejun</p>
+              <p className="text-hover">Dimineata nu aveti de grija, doar coborati la restaurant unde veti putea savura micul dejun</p>
+
+            </div>
+          </div>
+          <div className="gallery-item">
+            <img src={sauna} alt="Sauna" />
+            <div className="overlay">
+              <p className="text">Sauna</p>
+              <p className="text-hover">In zilele cu vreme urata afara, puteti sta in Sauna interioara a hotelului</p>
+
+            </div>
+          </div>
+        </div>
+        </div>
+      );
 }
 
-export default ImageGallery;
+export default Facilities;
