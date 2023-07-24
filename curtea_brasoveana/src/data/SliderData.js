621c97e05bbe09c1dc198b3523383e6e6537bbb1
teodor-daniel1234 <teodor-daniel.balan@s.unibuc.ro>
2023-07-24 19:52:34 +0300

    Add images
    Add images to frontpage

--- curtea_brasoveana/src/data/SliderData.js
+++ curtea_brasoveana/src/data/SliderData.js
@@ -1,6 +1,6 @@
 import ImageOne from '../assets/Interior.jpg'
 import ImageTwo from '../assets/Low.jpg'
-
+import ImageThree from '../assets/Night.jpeg'
 export const SliderData = [
     {
         title: 'O experienta de neuitat',
@@ -17,6 +17,14 @@ export const SliderData = [
         path: '/home',
         label: 'View Home',
         image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'Cioata de seara',
+        price: 'Ai cerut 1 ai primit 0.7',
+        path: '/home',
+        label: 'View Home',
+        image: ImageThree,
         alt: 'House'
     }
 ]
