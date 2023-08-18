7f4a423f1546ec9365080b7a4bd6b8a4c546828b
Catalin1901 <catacatalin19012003@yahoo.com>
2023-08-18 13:34:03 +0300

    Logo change

--- curtea_brasoveana/src/Components/Footer.js
+++ curtea_brasoveana/src/Components/Footer.js
@@ -2,6 +2,7 @@ import '../styles/footer.css';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faSquareWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import logo from "../assets/Logo_mic.png"
 
 const Footer = () => {
   const whatsappLink = 'https://api.whatsapp.com/send?phone=1234567890'; // Replace with your actual WhatsApp link
@@ -17,6 +18,7 @@ const Footer = () => {
         <div className="footer-social">
           <a href={facebookLink} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFacebookF} style={{color: "#005eff",}} size="3x" />          </a>
+          <img src={logo} alt="Logo" style={{ height:'50px', width: '50px' , marginTop: '-22px', marginRight: '1vw' }}  />
           <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faSquareWhatsapp} size="3x" style={{color: "#25d366",}} />
           </a>
