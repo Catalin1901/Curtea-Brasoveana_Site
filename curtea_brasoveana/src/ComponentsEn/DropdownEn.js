ff18a2e5fec13bed172ea66ffd828d1eb989abe5
teodor-daniel1234 <teodor-daniel.balan@s.unibuc.ro>
2023-08-19 12:41:53 +0300

    Update DropdownEn.js

--- curtea_brasoveana/src/ComponentsEn/DropdownEn.js
+++ curtea_brasoveana/src/ComponentsEn/DropdownEn.js
@@ -3,7 +3,7 @@ import styled , {css} from 'styled-components'
 import { Button } from './ButtonEn.js';
 import {FaTimes} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
-import { MenuDataEn } from '../data/MenuData.js';
+import { MenuDataEn } from '../data/MenuDataEn.js'
 import RoIcon from '../assets/Ro.svg'
 import EnIcon from '../assets/En.svg'
 const DropdownContainer = styled.div`
