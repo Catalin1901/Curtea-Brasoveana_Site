842b5440db9bbde78eec1ed0a931bea862ebe9e5
teodor-daniel1234 <teodor-daniel.balan@s.unibuc.ro>
2023-08-19 11:32:21 +0300

    Update alte pagini

--- curtea_brasoveana/src/Components/Contact.js
+++ curtea_brasoveana/src/Components/Contact.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import '../../src/styles/contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone, faLock, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+
+const Separator = styled.div`
+  height: ${(props) => props.space}px; /* Set the desired space using the 'space' prop */
+`;
 
 const mobilephone = '+40735472336';
 const phonenumber = '+40268472336';
@@ -80,6 +85,7 @@ function Contact() {
                 <div className="map-container">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.708639624832!2d25.57784427677371!3d45.63658912191936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b35b68a1ae9103%3A0x20c570eb379b5e75!2zQ3VydGVhIEJyYciZb3ZlYW7Egw!5e0!3m2!1sro!2sro!4v1690113036088!5m2!1sro!2sro"
                         className="map" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                        </div>
+                            <Separator space={10} />
 
             </div>
         </div>
